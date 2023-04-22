3ba80e6a6cbc2bc15965aaae6ba597f1e2550aee
owolfdev <owolfdev@gmail.com>
2023-04-22 08:19:39 +0700

    add github link

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,6 +4,11 @@ import Grid from "../components/Grid";
 const Home = () => {
   return (
     <div className="p-10">
+      <div className="mb-5 font-bold text-blue-600 hover:text-blue-400">
+        <a target="_blank" href="https://github.com/owolfdev/ag-grid-demo">
+          Github Repo
+        </a>
+      </div>
       <Grid />
     </div>
   );
